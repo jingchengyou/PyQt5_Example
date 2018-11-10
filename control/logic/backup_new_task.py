2fb4c22d4f6e524fc225c53e99a4f6b88da3f66d
jingchengyou <2505034080@qq.com>
2018-11-10 18:17:10 +0800

    添加backup_new_task.py中文件路径部分

--- control/logic/backup_new_task.py
+++ control/logic/backup_new_task.py
@@ -3,6 +3,34 @@
 config/backup中存放
 若没有，则在本地新建文件夹
 """
+import os
+from datetime import datetime
+
+folder = datetime.now().strftime('%Y%m%d')
+
+
+def init_folder():
+    path = 'D:\\JetBrains\\person\\old_to_new\\config\\backup'
+    if os.path.exists(path):
+        return path
+    else:
+        temp_path = os.getcwd() + '\\old_to_new\\config\\backup'
+        if not os.path.exists(temp_path):
+            os.makedirs(temp_path)
+        return temp_path
+
+
+class Backup:
+    def __init__(self, task: int, data: tuple):
+        self.task_name = str(task)
+        self.backup_data = data
+
+        self.file = init_folder() + '\\' + folder + '\\' + self.task_name
+
+    def backup_new_task(self):
+        pass
+
+
 if __name__ == "__main__":
     pass
 
